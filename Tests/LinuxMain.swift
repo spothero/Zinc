631414a0099f012b1a6accb57090b723611dec2a
Brian Drelling <brian.drelling@gmail.com>
2020-05-13 02:04:56 -0500

    Resolved more SwiftLint violations

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,10 +1,8 @@
 import XCTest
 
-import CommandHeroTests
 import ZincTests
 
 var tests = [XCTestCaseEntry]()
-tests += CommandHeroTests.__allTests()
 tests += ZincTests.__allTests()
 
 XCTMain(tests)
