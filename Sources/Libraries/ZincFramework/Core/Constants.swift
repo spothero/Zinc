4a6a4378f40ed1d9f81084aab4ecfcd158ae75ba
Brian Drelling <brian.drelling@gmail.com>
2021-01-12 11:47:14 -0600

    IOS-2699: Ran SwiftFormat to update header violations for new year

--- Sources/Libraries/ZincFramework/Core/Constants.swift
+++ Sources/Libraries/ZincFramework/Core/Constants.swift
@@ -1,4 +1,4 @@
-// Copyright © 2020 SpotHero, Inc. All rights reserved.
+// Copyright © 2021 SpotHero, Inc. All rights reserved.
 
 struct Constants {
     /// Denotes the default filenames to check if none are provided, in prioritized order.
